b45b1729ec9dd5ab3bcd278284f2f194b38f45d9
toluwani <alimsontoluwani@gmail.com>
2022-04-08 18:35:34 -0700

    tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - function begins
+ *
+ *return - function ends
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
