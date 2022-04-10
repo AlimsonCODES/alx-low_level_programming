59c7b9e510f3c798b7da2a0a05b09508b83941cc
toluwani <alimsontoluwani@gmail.com>
2022-04-10 12:37:51 -0700

    leggo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit base 10 numbers
+ *
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for (ch = '0'; ch = '9'; ch++)
+	       putchar(ch);
+	return (0);
+}
