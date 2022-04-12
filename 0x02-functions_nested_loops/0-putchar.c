be8d2d802336ecc9844e8d18f701551e172f8dda
toluwani <alimsontoluwani@gmail.com>
2022-04-12 08:16:42 -0700

    hush

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = ('_putchar')
+		putchar(n);
+	
+	putchar("\n");
+	return (0);
+}		
