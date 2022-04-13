33e2722bafdbc86000958fca14020106a837f357
toluwani <alimsontoluwani@gmail.com>
2022-04-13 14:39:20 -0700

    ok

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ *
+ * Return: Always 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c) != 0)
+	{
+		_putchar(c);
+	}
+	else
+	{
+		_putchar(c);
+	}
+}
