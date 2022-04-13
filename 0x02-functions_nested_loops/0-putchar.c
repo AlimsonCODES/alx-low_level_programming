305dfa49ea0ed56454495601c72dba2976d7ecc1
toluwani <alimsontoluwani@gmail.com>
2022-04-12 17:04:27 -0700

    lol

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,14 @@
 
 int main(void)
 {
-	int n = ('_putchar')
-		putchar(n);
-	
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar("\n");
 	return (0);
 }		
