a478c8fd9114e8575b8c42545b26cb653a9b1e04
toluwani <alimsontoluwani@gmail.com>
2022-04-14 14:54:50 -0700

    okl

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line.
- *
+ *@n: function
  *
  * Return: Always 0 (success)
  */
@@ -32,5 +32,4 @@ void print_to_98(int n)
 			_putchar('\n');
 		}
 	}
-	return (0);
 }
