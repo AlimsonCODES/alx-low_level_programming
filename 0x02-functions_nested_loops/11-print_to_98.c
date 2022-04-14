0913fc52829d9b4d5f97c3a41342122088e71630
toluwani <alimsontoluwani@gmail.com>
2022-04-14 15:05:42 -0700

    okay

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,25 +11,20 @@
 
 void print_to_98(int n)
 {
-	if (n < 0)
+	if (n >=  98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
-			_putchar('\n');
 		}
+	printf("%d\n", 98);
 	}
-	 else if (n == 98)
-	 {
-		 printf("%d. ", n);
-		 _putchar('\n');
-	 }
 	else
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
-			_putchar('\n');
 		}
+	printf("%d\n", 98);
 	}
 }
