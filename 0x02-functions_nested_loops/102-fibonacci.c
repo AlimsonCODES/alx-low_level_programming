303bc37305fbbba47f1dc4e15f461bcf71f9f7c9
toluwani <alimsontoluwani@gmail.com>
2022-04-14 19:38:28 -0700

    loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print first 50 fibonacci
  *
@@ -7,13 +8,14 @@
 
 int main(void)
 {
-	long int i, x =1, y = 2, sum = 0;
+	long int i, x = 1, y = 2, sum = 0;
+
 	for (i = 0; i < 49; i++)
 	{
 		printf("%ld, ", x);
 		sum = x + y;
 		x = y;
-		y =sum;
+		y = sum;
 		if (i == 48)
 			printf("%ld\n", x);
 	}
