f370315de076ea640acf5bf7e5432e4e89d20681
toluwani <alimsontoluwani@gmail.com>
2022-04-16 05:34:31 -0700

    opo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints fizz buzz
- * @n: function
+ * 
  * Return: 0
  */
 
@@ -13,15 +13,15 @@ int main(void)
 
 	for (n = 0; n <= 100; n++)
 	{
-		if (n % 3 = 0)
+		if (n % 3 == 0 && n % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 = 0)
+		else if (n % 5 == 0 && n % 5 != 0)
 		{
 			printf(" Buzz");
 		}
-		else if (n % 3 = 0 && n % 5 = 0)
+		else if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
@@ -34,6 +34,7 @@ int main(void)
 			printf(" %d", n);
 		}
 	}
+
 	_putchar('\n');
 	return (0);
 }
