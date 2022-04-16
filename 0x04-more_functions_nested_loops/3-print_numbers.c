f1210b0fdeca83dfec5596129e62e21e9f5a8e63
toluwani <alimsontoluwani@gmail.com>
2022-04-15 18:19:10 -0700

    omo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	int ch;
+int c;
 
-	for (ch = 0; ch <= 9; ch++)
-	{
-		_putchar(ch + '0');
-	}
-	_putchar('\n');
+for (c = 0; c <= 9; c++)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
 }
