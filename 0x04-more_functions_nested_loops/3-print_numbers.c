4c55a45e6bd73d6804aff3f66de2e0318e3b3fb5
toluwani <alimsontoluwani@gmail.com>
2022-04-15 17:41:20 -0700

    ol

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 through to 9
+ * @ch: holds values
+ * Return: 
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = 0; ch =< 9; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
