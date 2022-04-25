e4014c71a89374ea0a6a11ffd74b573cbda2f7f5
toluwani <alimsontoluwani@gmail.com>
2022-04-24 21:08:33 -0700

    ok

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(*dest *src);
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (dest[len] != '\0')
+	{
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
 	return (dest);
 }
