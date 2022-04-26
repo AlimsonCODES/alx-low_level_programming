fab2ba3e7bcdd8996054fe3f64e0d5c5053066c8
toluwani <alimsontoluwani@gmail.com>
2022-04-25 19:52:33 -0700

    opo

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,262 @@
+#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}#include "main.h"
+
+/**
+ * _strncat - func
+ * @dest: func
+ * @src: func
+ * @n: func
+ * Return: 
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (len2 != n)
+	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
+		*(dest + len) = src[len2];
+		len++;
+		len2++;
+	}
+	return (dest);
+}
