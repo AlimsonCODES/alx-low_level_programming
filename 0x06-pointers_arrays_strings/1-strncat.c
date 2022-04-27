3d80ff9c427afa5e6d697015efb89289d73b6ff2
toluwani <alimsontoluwani@gmail.com>
2022-04-26 19:59:02 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat - func
- * @dest: func
- * @src: func
- * @n: func
- * Return: 
+ * _strncat - function that concatenates two strings.
+ * @dest: func 1
+ * @src: func 3
+ * @n: func 34
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -14,7 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	int len2 = 0;
 
 	while (dest[len] != '\0')
+	{
 		len++;
+	}
 	while (len2 != n)
 	{
 		if (src[len2] == '\0')
