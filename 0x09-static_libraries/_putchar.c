62dfc2bd9dc4d223c5e142f72f50a3b0ccf22b97
toluwani <alimsontoluwani@gmail.com>
2022-05-03 20:55:48 -0700

    lets see

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints putchar
- *
+ * _putchar - prints putchar
+ * @c: parm
  * Return: Always 0 (success)
  */
 
 int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	return (write(1, &c, 1));
 }
