0a7224984209c5652465c45fb2ecd1b867df2b36
toluwani <alimsontoluwani@gmail.com>
2022-05-05 22:13:00 -0700

    ok

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: count
+ * @argv: str
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
