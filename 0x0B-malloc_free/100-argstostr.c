08cbfa0eac0c96a6a90f28e6b121cef6ab6e49f2
toluwani <alimsontoluwani@gmail.com>
2022-05-06 21:10:52 -0700

    ol

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,38 +7,32 @@
  * @av: param
  * Return: pointer
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *arg;
 	int c, i, j, k;
 
-
 	if (ac == 0)
 		return (NULL);
-
 
 	for (c = i = 0; i < ac; i++)
 	{
 		if (av[i] == NULL)
 			return (NULL);
-
 	
 		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
 		c++;
 	}
 
-
 	arg = malloc((c + 1) * sizeof(char));
-
 
 	if (arg == NULL)
 	{
 		free(arg);
-
 		return (NULL);
 	}
-
 
 	for (i = j = k = 0; k < c; j++, k++)
 	{
@@ -53,7 +47,6 @@ char *argstostr(int ac, char **av)
 			arg[k] = av[i][j];
 	}
 	arg[k] = '\0';
-
 
 	return (arg);
 }
