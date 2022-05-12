9d2966640d8c79fca7a386ea5c370efc47091633
toluwani <alimsontoluwani@gmail.com>
2022-05-11 21:47:28 -0700

    yh

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - int dog obj
+ * @d: poiter para
+ * @name: pointer param
+ * @age: param
+ * @owner: pointer param
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		d->owner = owner;
+	}
+}
