412a57bd7d28e39eb293abbbd658d9d07ab595cd
toluwani <alimsontoluwani@gmail.com>
2022-05-16 22:13:20 -0700

    kl

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,6 +5,7 @@
  * @separator: pointer param
  * @n: param
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
@@ -16,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
-		
+
 		if (str)
 			printf("%s", str);
 		else
