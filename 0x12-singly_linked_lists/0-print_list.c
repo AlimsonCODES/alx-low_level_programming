cb009a91cab470b735a2276ac97e4b9c4e2c0661
Alimson <alimsontoluwani@gmail.com>
2022-05-26 20:05:04 -0700

    001

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_list - prints parameters
+ * @h: address to head
+ * Return: number of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodelen = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s", 0, "nil");
+		else
+			printf("[%d] %s", h->len, h->str);
+
+		h = h->next;
+		nodelen++;
+	}
+	return (nodelen);
+}
+
