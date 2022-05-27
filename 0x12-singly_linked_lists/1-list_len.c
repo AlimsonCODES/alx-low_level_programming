66c1a65abc172664ed67ef3899ac5e19374633ee
Alimson <alimsontoluwani@gmail.com>
2022-05-27 02:21:32 -0700

    task 1 test 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - list amounts of elements
+ * @h: parameter
+ * Return: struct
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n;
+	n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
