e875e6c3f5dedd21da4b03d6f779f6af5ad4983b
Alimson <alimsontoluwani@gmail.com>
2022-05-27 04:10:36 -0700

    redo lsat commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,13 @@ size_t node_len = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
+{
 printf("[%d] %s\n", 0, "nil");
+}
 else
+{
 printf("[%d] %s\n", h->len, h->str);
+}
 h = h->next;
 nodelen++;
 }
