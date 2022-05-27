5143cee8875d8ea7c246de37cda5f4bc4eb70b5b
Alimson <alimsontoluwani@gmail.com>
2022-05-27 04:07:03 -0700

    task 0, test 3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodelen = 0;
-
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "nil");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nodelen++;
-	}
-	return (nodelen);
+size_t node_len = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "nil");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nodelen++;
+}
+return (nodelen);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len - list amounts of elements
+ * list_len - list amounts of ele
  * @h: parameter
  * Return: struct
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t n;
-	n = 0;
-	while (h != NULL)
-	{
-		h = h->next;
-		n++;
-	}
-	return (n);
+size_t n;
+n = 0;
+while (h != NULL)
+{
+h = h->next;
+n++;
+}
+return (n);
 }
