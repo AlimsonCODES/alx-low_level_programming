81bee129400b4ebbc857798c02e8470e2ab170a0
Alimson <alimsontoluwani@gmail.com>
2022-05-27 23:58:24 -0700

    hm2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
 /**
@@ -20,27 +19,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-void free_listint2(listint_t **head);
-void free_listint(listint_t *head);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-int pop_listint(listint_t **head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-
-//Advanced tasks prototypes and stucts
-
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *reverse_listint(listint_t **head);
-listint_t *find_listint_loop(listint_t *head);
-
-/**
+/** 
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
@@ -53,5 +32,22 @@ typedef struct listp_s
 	void *p;
 	struct listp_s *next;
 } listp_t;
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+void free_listint2(listint_t **head);
+void free_listint(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+int pop_listint(listint_t **head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *reverse_listint(listint_t **head);
+listint_t *find_listint_loop(listint_t *head);
+
 
 #endif
