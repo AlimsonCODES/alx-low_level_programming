a1333acc1a60b269378039a5c1c0f18958ff712c
toluwani <alimsondaniel@gmail.com>
2022-08-02 20:52:07 -0700

    lets see

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - check how many nodes
+ * @h: pointer to head
+ * Return: len
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len;
+	len = 0;
+
+	if (h == NULL)
+		return (len);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
