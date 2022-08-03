0e72659235e56ad336bdd4e6a7b28aaeb6af0fb5
toluwani <alimsondaniel@gmail.com>
2022-08-02 20:03:47 -0700

    commit 001

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,20 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t len = 0;
-	while (h->next != NULL)
+	int len = 0;
+
+	if (h == NULL)
+		return (len);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		len++;
+		h = h->next;
 	}
-	printf("-> %ld elements\n", len);
+	printf("-> %d elements\n", len);
 	return (len);
 }
